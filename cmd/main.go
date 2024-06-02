2818b8cd99ad726849da7877f23f75eda23e0c1a
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-02 16:08:58 +0800

    feat: add discord bot session

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,39 @@ package main
 
 import (
 	"github.com/jallenmanaloto/soha-bot/config"
-	// "github.com/jallenmanaloto/soha-bot/internal/database"
+	"github.com/jallenmanaloto/soha-bot/internal/bot"
+	"github.com/jallenmanaloto/soha-bot/internal/database"
 	"github.com/jallenmanaloto/soha-bot/internal/server"
 	"github.com/jallenmanaloto/soha-bot/pkg/logger"
 )
 
 func main() {
+	logger.Log.Info("INFO starting Application")
+
 	// initialize aws config and load env variables
 	config.NewClientConfig()
 
-	// db := database.New()
+	// starting new db instance
+	db := database.New()
 
-	logger.Log.Info("INFO starting Application")
+	// initialize bot session
+	logger.Log.Info("INFO starting Soha discord bot")
+	bot, err := bot.New(db)
+	if err != nil {
+		logger.Log.Errorf("ERROR failed to initialize discord bot: %v", err)
+	}
+
+	err = bot.Session.Open()
+	if err != nil {
+		logger.Log.Errorf("ERROR failed to open discord bot session: %v", err)
+	}
+
+	// starting server for http reqs
+	logger.Log.Info("INFO starting http server")
 	server := server.NewServer()
-
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		logger.Log.Errorf("ERROR unable to start server: %v\n", err)
 		return
 	}
-
-	logger.Log.Info("INFO server started...")
 }
