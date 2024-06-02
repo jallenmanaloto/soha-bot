c784fbd82cea6350b135a0847acc44b2b4db791d
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-02 14:48:56 +0800

    feat: initialize aws sdk configs

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,18 @@ package main
 import (
 
 	// "github.com/jallenmanaloto/soha-bot/internal/database"
+
+	"github.com/jallenmanaloto/soha-bot/config"
 	"github.com/jallenmanaloto/soha-bot/internal/server"
 	"github.com/jallenmanaloto/soha-bot/pkg/logger"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// initialize aws config and load env variables
+	config.NewClientConfig()
+	config.LoadEnvironmentVariables()
+
 	if err := godotenv.Load(); err != nil {
 		logger.Log.Errorf("ERROR unable to load env variables: %v\n", err)
 		return
@@ -17,6 +23,7 @@ func main() {
 
 	logger.Log.Info("INFO starting Application")
 	server := server.NewServer()
+
 	err := server.ListenAndServe()
 	if err != nil {
 		logger.Log.Errorf("ERROR unable to start server: %v\n", err)
