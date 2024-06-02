0e71a72468201bfdd05e30867d2e04ef55657335
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-02 12:40:47 +0800

    feat: used a constant port

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,29 +3,24 @@ package server
 import (
 	"fmt"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
-
-	"github.com/jallenmanaloto/soha-bot/pkg/logger"
 )
 
 type Server struct {
 	port int
 }
 
-func NewServer() *http.Server {
-	port, err := strconv.Atoi(os.Getenv("PORT"))
-	if err != nil {
-		logger.Log.Errorf("ERROR unable to convert PORT to string: %v\n", err)
-	}
+const (
+	ServerPort = 8000
+)
 
+func NewServer() *http.Server {
 	NewServer := &Server{
-		port: port,
+		port: ServerPort,
 	}
 
 	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", NewServer.port),
+		Addr:         fmt.Sprintf(":%d", ServerPort),
 		Handler:      NewServer.RegisterRoutes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
