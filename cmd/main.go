9255d95ba4f614b49a4b135dce614a78b18154fd
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-02 15:42:11 +0800

    refactor: removed loading of env vars

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/jallenmanaloto/soha-bot/config"
 	// "github.com/jallenmanaloto/soha-bot/internal/database"
 	"github.com/jallenmanaloto/soha-bot/internal/server"
 	"github.com/jallenmanaloto/soha-bot/pkg/logger"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	// initialize aws config and load env variables
 	config.NewClientConfig()
-	config.LoadEnvironmentVariables()
-	port := os.Getenv("PORT")
-	fmt.Printf("PORT value: %v\n", port)
 
-	if err := godotenv.Load(); err != nil {
-		logger.Log.Errorf("ERROR unable to load env variables: %v\n", err)
-		return
-	}
 	// db := database.New()
 
 	logger.Log.Info("INFO starting Application")
--- config/config.go
+++ config/config.go
@@ -3,11 +3,9 @@ package config
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
 var Config aws.Config
@@ -20,20 +18,4 @@ func NewClientConfig() {
 		fmt.Printf("ERROR Unable to load AWS SDK config: %v\n", err)
 	}
 	Config = cfg
-}
-
-func LoadEnvironmentVariables() {
-	parameters := []string{"PORT", "LOKI_LOG_URL", "LOKI_USERNAME", "LOKI_PASSWORD"}
-	svc := ssm.NewFromConfig(Config)
-
-	for _, parameter := range parameters {
-		paramOut, err := svc.GetParameter(context.TODO(), &ssm.GetParameterInput{
-			Name:           aws.String(parameter),
-			WithDecryption: aws.Bool(true),
-		})
-		if err != nil {
-			fmt.Printf("Unable to get parameters: %v", err)
-		}
-		os.Setenv(parameter, *paramOut.Parameter.Value)
-	}
 }
