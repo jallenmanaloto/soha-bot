d17ed1587030f8168664269b8fd356681bf5a71d
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-03 13:19:41 +0800

    feat: created method to search manhwa by title

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,12 +1,22 @@
 package database
 
 import (
+	"context"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	cfg "github.com/jallenmanaloto/soha-bot/config"
+	"github.com/jallenmanaloto/soha-bot/internal/constants"
+	"github.com/jallenmanaloto/soha-bot/models"
+	"github.com/jallenmanaloto/soha-bot/pkg/logger"
 )
 
 type Service struct {
-	db *dynamodb.Client
+	db        *dynamodb.Client
+	TableName string
 }
 
 var (
@@ -18,9 +28,49 @@ func New() *Service {
 		return dbInstance
 	}
 
+	table := os.Getenv("TABLENAME")
 	ddbClient := dynamodb.NewFromConfig(cfg.Config)
 	dbInstance = &Service{
-		db: ddbClient,
+		db:        ddbClient,
+		TableName: table,
 	}
 	return dbInstance
+}
+
+func SearchManhwa(title string) ([]models.Manhwa, error) {
+	var manhwas []models.Manhwa
+	var err error
+	var response *dynamodb.ScanOutput
+
+	input := &dynamodb.ScanInput{
+		TableName: aws.String(dbInstance.TableName),
+	}
+
+	filtEx := expression.Name("Title").Contains(title)
+	expr, err := expression.NewBuilder().WithFilter(filtEx).Build()
+	if err != nil {
+		logger.Log.Errorf(constants.ErrorBuildExpression, err)
+		return nil, err
+	}
+	input.ExpressionAttributeNames = expr.Names()
+	input.ExpressionAttributeValues = expr.Values()
+	input.FilterExpression = expr.Filter()
+
+	response, err = dbInstance.db.Scan(context.Background(), input)
+	if err != nil {
+		logger.Log.Errorf(constants.ErrorScan, err)
+		return nil, err
+	}
+
+	for _, item := range response.Items {
+		var manhwa models.Manhwa
+		err = attributevalue.UnmarshalMap(item, &manhwa)
+		if err != nil {
+			logger.Log.Errorf(constants.ErrorUnmarshalItem, err)
+			return nil, err
+		}
+		manhwas = append(manhwas, manhwa)
+	}
+
+	return manhwas, nil
 }
