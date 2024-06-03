65789ccef1ce75c53838c22a63c5d959b57edbd7
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-03 13:24:22 +0800

    feat: created Manhwa model

---
+++ models/models.go
@@ -0,0 +1,12 @@
+package models
+
+type Manhwa struct {
+	PK         string
+	SK         string
+	Chapters   string
+	Image      string
+	Rating     string
+	Scanlation string
+	Title      string
+	Url        string
+}
