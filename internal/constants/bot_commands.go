d916bd5bd76db899d1210040dcab60cdbf9f16b4
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-03 14:18:49 +0800

    refactor: update default message

--- internal/constants/bot_commands.go
+++ internal/constants/bot_commands.go
@@ -14,8 +14,8 @@ const (
 // Command messages
 const (
 	MessageDefault          = "I don't know that trick! Just give me treats, you piece of shit!"
-	MessageFetch            = "`!soha fetch:` Soha will fetch all the titles he is watching for any latest chapters."
-	MessageHello            = "arf arf!"
+	MessageFetch            = "`!soha fetch:` Soha will fetch all the titles he is watching for you."
+	MessageHello            = "arf arf! What is it, you degenerate!"
 	MessageLook             = "`!soha look <title>:` Soha to look for a manhwa with that title."
 	MessageTricks           = "Soha's tricks or command displays things he can do.\nYou can call out to Soha with `!soha` followed by your command\n\n%s\n\n%s\n\n%s"
 	MessageTricksEmbedTitle = "**Soha's tricks and quirks**"
