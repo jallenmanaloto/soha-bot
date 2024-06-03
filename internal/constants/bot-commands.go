6d9e0ab9c1f79129cdfe7d59cb2c3d8185b6d47a
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-03 15:52:55 +0800

    feat: change file name to bot-commands

--- internal/constants/bot-commands.go
+++ internal/constants/bot-commands.go
@@ -7,7 +7,7 @@ const (
 	Fetch   = "fetch"
 	Hello   = "hello"
 	Look    = "look"
-	Prefix  = "!soha"
+	Prefix  = "!test"
 	Tricks  = "tricks"
 	Watch   = "watch"
 )
@@ -18,7 +18,14 @@ const (
 	MessageFetch            = "`!soha fetch:` Soha will fetch all the titles he is watching for you."
 	MessageHello            = "arf arf! What is it, you degenerate!"
 	MessageLook             = "`!soha look <title>:` Soha to look for a manhwa with that title."
+	MessageManhwaNotExist   = "I can't find that manhwa. You're wasting my energy you shit!"
 	MessageTricks           = "Soha's tricks or command displays things he can do.\nYou can call out to Soha with `!soha` followed by your command\n\n%s\n\n%s\n\n%s"
 	MessageTricksEmbedTitle = "**Soha's tricks and quirks**"
-	MessageWatch            = "`!soha watch <title>:` Soha will watch out for new chapters for the title."
+	MessageWatch            = "`!soha watch <ID>:` Soha will watch out for new chapters for the title."
+	MessageWatchSuccess     = "arf! I am now watching that for you, you 'lil shit."
+)
+
+// Message embed
+const (
+	EmbedManhwaDesc = "ID: %s\n%s"
 )
