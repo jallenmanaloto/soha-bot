89729e9406fab8949b95e7f6715a72275005de39
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-03 14:29:45 +0800

    feat: create utils to extract url & title

---
+++ internal/bot/utils/extract.go
@@ -0,0 +1,15 @@
+package utils
+
+import "strings"
+
+func ExtractTitle(param []string) string {
+	p := param[2:]
+	title := strings.Join(p, " ")
+	return title
+}
+
+func ExtractUrl(param string) string {
+	split := strings.SplitAfter(param, "(webp)/")
+	url := split[1]
+	return url
+}
