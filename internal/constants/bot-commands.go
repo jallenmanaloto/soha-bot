972c0a9ce0242d32d1bda1a439e3b8a10adc271c
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-04 14:03:08 +0800

    feat: add message for watch list

--- internal/constants/bot-commands.go
+++ internal/constants/bot-commands.go
@@ -20,13 +20,16 @@ const (
 	MessageHello            = "arf arf! What is it, you degenerate!"
 	MessageLook             = "`!soha look <title>:` Soha to look for a manhwa with that title."
 	MessageManhwaNotExist   = "I can't find that manhwa. You're wasting my energy you shit!"
+	MessageEmptyWatchList   = "arf! arf! arf! I am not watching anything yet. Give me treats and I'll watch anything for you dumdum!"
 	MessageTricks           = "Soha's tricks or command displays things he can do.\nYou can call out to Soha with `!soha` followed by your command\n\n%s\n\n%s\n\n%s"
 	MessageTricksEmbedTitle = "**Soha's tricks and quirks**"
 	MessageWatch            = "`!soha watch <ID>:` Soha will watch out for new chapters for the title."
+	MessageWatchListTitle   = "arf! arf! These are the titles I am watchin' for you shit."
 	MessageWatchSuccess     = "arf! I am now watching that for you, you 'lil shit."
 )
 
 // Message embed
 const (
-	EmbedManhwaDesc = "ID: %s\n%s"
+	EmbedManhwaDesc      = "ID: %s\n%s"
+	EmbedManhwaWatchList = "**%s**ID: %s\n%s\n%s\n\n" // format: Title, ID, Chapter, Url
 )
