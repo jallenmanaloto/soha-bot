1ac4c6d8b8726edd446c1a49de60990fffe27324
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-04 12:45:04 +0800

    feat: create GenerateKey method for PK and SK

--- internal/bot/utils/form-manhwa.go
+++ internal/bot/utils/form-manhwa.go
@@ -7,12 +7,24 @@ import (
 	"github.com/jallenmanaloto/soha-bot/models"
 )
 
+func GenerateKey(keyType string, val string) (string, string) {
+	var pk string
+	var sk string
+
+	if keyType == constants.ServerPK {
+		pk = constants.ServerPK
+		sk = fmt.Sprintf(constants.ServerSK, val)
+	}
+
+	return pk, sk
+}
+
 func FormServerManhwa(manhwa models.Manhwa, gid string, chId string) models.ServerManhwa {
-	servrSK := fmt.Sprintf(constants.ServerSK, gid)
+	pk, sk := GenerateKey("SERVER", gid)
 
 	serverManhwa := &models.ServerManhwa{
-		PK:       constants.ServerPK,
-		SK:       servrSK,
+		PK:       pk,
+		SK:       sk,
 		ChanId:   chId,
 		ServerId: gid,
 		TitleId:  manhwa.ID,
