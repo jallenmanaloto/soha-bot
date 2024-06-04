403fb14841ebf08b631e912d9f9c6b809bfd506d
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-04 14:52:10 +0800

    feat: create new generate id util

---
+++ internal/bot/utils/generate-id.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func GenerateId() (string, error) {
+	var id string
+
+	bytes := make([]byte, 16)
+	if _, err := rand.Read(bytes); err != nil {
+		return id, err
+	}
+	return hex.EncodeToString(bytes), nil
+}
