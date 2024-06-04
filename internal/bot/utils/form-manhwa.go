80cda56603eafb1a43546b0c74da267b44675bab
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-04 11:55:34 +0800

    feat: use gid for SK and add title id

--- internal/bot/utils/form-manhwa.go
+++ internal/bot/utils/form-manhwa.go
@@ -2,21 +2,20 @@ package utils
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/jallenmanaloto/soha-bot/internal/constants"
 	"github.com/jallenmanaloto/soha-bot/models"
 )
 
 func FormServerManhwa(manhwa models.Manhwa, gid string, chId string) models.ServerManhwa {
-	title := strings.ReplaceAll(manhwa.Title, " ", "_")
-	servrSK := fmt.Sprintf(constants.ServerSK, title)
+	servrSK := fmt.Sprintf(constants.ServerSK, gid)
 
 	serverManhwa := &models.ServerManhwa{
 		PK:       constants.ServerPK,
 		SK:       servrSK,
 		ChanId:   chId,
 		ServerId: gid,
+		TitleId:  manhwa.ID,
 		Title:    manhwa.Title,
 	}
 	return *serverManhwa
