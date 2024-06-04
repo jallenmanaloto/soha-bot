55457a8da248277c2f447aa35f67946982b8e37f
jallenmanaloto <jallen.manaloto@gmail.com>
2024-06-04 16:49:37 +0800

    feat: create webhook handler

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,13 +3,16 @@ package server
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 
+	"github.com/jallenmanaloto/soha-bot/internal/constants"
 	"github.com/jallenmanaloto/soha-bot/pkg/logger"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", s.Health)
+	mux.HandleFunc("POST /webhook/alert", s.UpdateAlert)
 
 	return mux
 }
@@ -25,4 +28,50 @@ func (s *Server) Health(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_, _ = w.Write(res)
+}
+
+type Alert struct {
+	Signature string `json:"signature"`
+	Title     string `json:"title"`
+	ManhwaId  string `json:"manhwaId"`
+}
+
+type AlertResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+func (s *Server) UpdateAlert(w http.ResponseWriter, r *http.Request) {
+	var alert Alert
+	sigKey := os.Getenv("SCRAPER_KEY")
+	err := json.NewDecoder(r.Body).Decode(&alert)
+	if err != nil {
+		logger.Log.Errorf(constants.ErrorJsonDecode, err)
+	}
+
+	if sigKey != alert.Signature {
+		alertRes := &AlertResponse{
+			Status:  401,
+			Message: constants.Unauthorized,
+		}
+		res, err := json.Marshal(alertRes)
+		if err != nil {
+			logger.Log.Errorf(constants.ErrorMarshalRes, err)
+		}
+		_, _ = w.Write(res)
+		return
+	}
+
+	// start operation
+	// go routine to send discord mess & send json
+
+	mes := &AlertResponse{
+		Status:  200,
+		Message: "Accepted",
+	}
+	succ, err := json.Marshal(mes)
+	if err != nil {
+		logger.Log.Errorf(constants.ErrorMarshalRes, err)
+	}
+	_, _ = w.Write(succ)
 }
